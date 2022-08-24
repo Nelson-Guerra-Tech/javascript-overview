da2f5997ef37ed94a1717046fdfc595a40b0ce26
Nelson-Guerra <nelsonguerra.tech@outlook.com>
2022-08-24 17:45:51 -0400

    Strings

---
+++ Chapter 2/sandbox.js
@@ -0,0 +1,33 @@
+// strings
+console.log('hello world')
+
+let email = 'nelsonguerra.tech@outlook.com'
+console.log(email);
+
+// string concatenation
+// fance word for joining things together
+let firstName = 'Nelson';
+let lastName = 'Guerra';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[1]);
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length)
+
+// string methods
+// function is a snippet of code that does something
+// a method is a piece of code that is attached to an element.
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+// passing a value into a method is called an argument
+let index = email.indexOf('@');
+console.log(index);
